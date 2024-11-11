8389c9a25e4d8d5f8782fe5a7aec6533b3e75f15
matheusperches <matheusperches359@gmail.com>
2024-11-11 19:29:04 -0300

    Updated folder structure

--- SpendSmart/obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
+++ SpendSmart/obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ece3832398cd40ac9e411985e563fb5fd96f717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da445d1c99eb708cbe20688ae0c2dc9599a4b9f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
